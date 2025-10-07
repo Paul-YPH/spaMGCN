db6ce812fba96f5e51809dc663e09b8c2fe940f1
Penghui Yang <59365168+Paul-YPH@users.noreply.github.com>
2025-10-07 19:58:25 -0400

    Update

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="spaMGCN",
+    version="77dfe67",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=[],
+    python_requires='>=3.7',
+)
